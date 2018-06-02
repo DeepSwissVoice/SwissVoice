ac02ce99d89de0a1e27698cae9dc8f8f872d4100
Simon Berger <siku2@outlook.de>
2018-06-02 20:43:41 +0200

    fancy new build file

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
-echo "Simon's super starter"
 @echo off
-call npm run-script build
+echo Simon's super starter
+echo ===========================
+echo   - Building static files...
+call npm run-script build >nul
+echo   - Server static files built
+echo   - Composing Container...
 cd server
-docker-compose up -d --build
+docker-compose up -d --build >nul
+echo   - Docker Container should be running!
 pause

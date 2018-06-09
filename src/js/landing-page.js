664d4d047b93520bd0ae3ff7f042fa49506f4731
thieme55 <thieme55@users.noreply.github.com>
2018-06-09 22:09:45 +0200

    Some loading screen stuff/fixes/ maybe fix

--- src/js/landing-page.js
+++ src/js/landing-page.js
@@ -1,11 +1,26 @@
+import $ from "jquery";
+import Raven from "raven-js";
+
+
 async function init() {
     // This function will be called as soon as the page has been loaded (do whatever you want!)
+    loadingAnimation();
 }
 
 function _init() {
     // If you need to do any initialisation before the page has been properly loaded, put the code in here
     $(init);
 }
+
+
+function loadingAnimation() {
+    setTimeout(function() {$('fly-text').removeClass('hidden')}, 500);
+    setTimeout(function() {
+    $('.loading-screen').addClass('loaded');
+    $('.fly-text').addClass('loaded')}, 2800);
+}
+
+
 
 Raven.config("https://23dcfd51df56440486089720f7184663@sentry.io/1214965", {
     release: VERSION

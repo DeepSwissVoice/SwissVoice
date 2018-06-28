4dbf5151b979398700a3d9364d0907ba98d41a8b
Simon Berger <siku2@outlook.de>
2018-06-29 00:02:26 +0200

    checking for proper text format

--- src/js/pages/schreiben.js
+++ src/js/pages/schreiben.js
@@ -1,5 +1,6 @@
 import SwissVoiceAPI from "../api";
 import setup from "../page-setup";
+import {sleep} from "../utils";
 
 const {elements} = setup({
     elements: {
@@ -14,13 +15,15 @@ const {elements} = setup({
 async function proposeTexts() {
     const content = elements.proposeTextsInput.val();
     const texts = content.split(";").map((s) => s.trim());
-    if (texts.slice(-1) === "." || "!" || "?"){
+    for (const text of texts) {
+        if (!text.match(/\w([?!.])$/g)) {
+            elements.proposeTextsInput.val("Bitte das Satzzeichen korrigieren");
+            await sleep(500);
+            elements.proposeTextsInput.val(texts);
+            return;
+        }
+    }
     const result = await SwissVoiceAPI.proposeTexts(...texts);
     elements.proposeTextsInput.val('');
     console.log(result);
-    } else {
-        elements.proposeTextsInput.val('Bitte das Satzzeichen korrigieren');
-        await sleep(500);
-        elements.proposeTextsInput.val(texts);
-    }
 }

afdb74ce20dda7ea284dfde552c7ed88517ae34b
Simon Berger <siku2@outlook.de>
2018-06-28 21:46:30 +0200

    gc

--- src/js/pages/schreiben.js
+++ src/js/pages/schreiben.js
@@ -1,29 +1,15 @@
-import(/* webpackChunkName: "bootstrap" */ "bootstrap");
-
 import SwissVoiceAPI from "../api";
 import setup from "../page-setup";
-import {nextRecordText, record} from "../record";
-import {nextSample, togglePlay, voteSample} from "../vote";
-
-let cantonsOverlayVisible = false;
 
 const {elements} = setup({
     onReady: init,
     elements: {
-        voteSampleButtons: "#vote-sample-true-btn, #vote-sample-false-btn",
         proposeTextsInput: "#propose-texts-input",
     },
     buttons: {
-        "#toggle-play-btn": togglePlay,
-        "#vote-sample-true-btn": () => voteSample(true),
-        "#vote-sample-false-btn": () => voteSample(false),
-        "#record-btn": record,
-        "#submit-proposed-texts": proposeTexts,
-        ".overlay-button-toggler": toggleOverlay
+        "#submit-proposed-texts": proposeTexts
     }
 });
-
-export {elements};
 
 
 async function proposeTexts() {
@@ -31,16 +17,4 @@ async function proposeTexts() {
     const texts = content.split(";").map((s) => s.trim());
     const result = await SwissVoiceAPI.proposeTexts(...texts);
     console.log(result);
-}
-
-
-async function init() {
-    if (SwissVoiceAPI.canton()) {
-        displayCantonFlag();
-        nextSample();
-        nextRecordText();
-    } else {
-        alert("Api isn't setup yet...");
-    }
-    displayCantons();
 }

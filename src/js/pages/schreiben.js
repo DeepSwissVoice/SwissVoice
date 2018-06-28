da4050216c93633b22180033041c78a8217e670d
thieme55 <thieme55@users.noreply.github.com>
2018-06-28 23:54:33 +0200

    text prop. btn function

--- src/js/pages/schreiben.js
+++ src/js/pages/schreiben.js
@@ -3,10 +3,10 @@ import setup from "../page-setup";
 
 const {elements} = setup({
     elements: {
-        proposeTextsInput: "#propose-texts-input",
+        proposeTextsInput: "#textarea-input",
     },
     buttons: {
-        "#submit-proposed-texts": proposeTexts
+        "#send-text": proposeTexts
     }
 });
 
@@ -14,6 +14,13 @@ const {elements} = setup({
 async function proposeTexts() {
     const content = elements.proposeTextsInput.val();
     const texts = content.split(";").map((s) => s.trim());
+    if (texts.slice(-1) === "." || "!" || "?"){
     const result = await SwissVoiceAPI.proposeTexts(...texts);
+    elements.proposeTextsInput.val('');
     console.log(result);
+    } else {
+        elements.proposeTextsInput.val('Bitte das Satzzeichen korrigieren');
+        await sleep(500);
+        elements.proposeTextsInput.val(texts);
+    }
 }

ffb77249544c8a7f666d5158c5cc2b8bfc6f708d
thieme55 <thieme55@users.noreply.github.com>
2018-07-05 22:38:08 +0200

    some layout changes
    - fixing layout
    - fixing expandable text

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,1 +1,27 @@
 import(/* webpackChunkName: "bootstrap" */ "bootstrap");
+
+import setup from "../page-setup";
+
+const {elements} = setup({
+    onReady: init,
+    requireCanton: false,
+    elements: {
+
+    },
+    buttons: {
+        ".teaser-trigger": (event) => {
+            const target = $(event.delegateTarget).prev();
+            if (target.hasClass("teaser")) {
+                target.attr("data-height", target.height());
+                target.height(target.prop("scrollHeight"));
+            } else {
+                target.height(target.attr("data-height"));
+            }
+            target.toggleClass("teaser");
+        }
+    }
+});
+
+async function init() {
+    
+}

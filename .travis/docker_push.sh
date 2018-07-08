632f88dba969203bb3e7de9666f9bda2c705df96
Simon Berger <siku2@outlook.com>
2018-07-08 23:17:57 +0200

    adding travis badge and fixing bash code... (12)

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    DOCKER_TAG = "latest"
-elif [ "$TRAVIS_BRANCH" == "development" ]; then
-    DOCKER_TAG = "dev"
+    DOCKER_TAG="latest"
+elif [ "$TRAVIS_BRANCH"= = "development" ]; then
+    DOCKER_TAG="dev"
 elif [ "$TRAVIS_BRANCH" == "travis" ]; then
-    DOCKER_TAG = "test"
+    DOCKER_TAG="test"
 fi
 
-if [ -z "$DOCKER_TAG" ]; then
+if [ -n "$DOCKER_TAG" ]; then
     echo "Deploying branch $TRAVIS_BRANCH with tag $DOCKER_TAG"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker push "$DOCKER_REPO":"$DOCKER_TAG"

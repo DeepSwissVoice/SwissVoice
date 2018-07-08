8f55cad295423ef11b6c52cd068f852e2b10a4f4
Simon Berger <siku2@outlook.com>
2018-07-08 23:02:42 +0200

    expanding docker_push.sh (10)

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$DOCKER_REPO"
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    DOCKER_TAG = "latest"
+elif [ "$TRAVIS_BRANCH" == "development" ]; then
+    DOCKER_TAG = "dev"
+elif [ "$TRAVIS_BRANCH" == "travis" ]; then
+    DOCKER_TAG = "test"
+fi
+
+if [ -z "$DOCKER_TAG" ]; then
+    echo "Deploying branch $TRAVIS_BRANCH with tag $DOCKER_TAG"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker push "$DOCKER_REPO":"$DOCKER_TAG"
+fi

6b4492b71b36025684413091470083c668af6690
Simon Berger <siku2@outlook.com>
2018-07-08 23:26:00 +0200

    creating tag and actually ignoring static files (13)

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -11,5 +11,6 @@ fi
 if [ -n "$DOCKER_TAG" ]; then
     echo "Deploying branch $TRAVIS_BRANCH with tag $DOCKER_TAG"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker push "$DOCKER_REPO":"$DOCKER_TAG"
+    docker tag "$DOCKER_REPO" "$DOCKER_REPO:$DOCKER_TAG"
+    docker push "$DOCKER_REPO:$DOCKER_TAG"
 fi
